84ae83cb6fac10906e5f9286c19bbc5b15639724
savsasa16 <67768303+savsasa16@users.noreply.github.com>
2025-05-30 23:20:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -990,6 +990,7 @@ def import_wheels_action():
         return redirect(url_for('export_import', tab='wheels_excel'))
 
 if __name__ == '__main__':
-    setup_database() 
+    with app.app_context():
+        setup_database() 
     port = int(os.environ.get('PORT', 5000)) # บรรทัดนี้สำคัญมาก
     app.run(host='0.0.0.0', port=port, debug=True)
